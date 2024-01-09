90e4bc0809230c0e2d2f43033599cba2a12d7cc8
up1 <somkiat.p@gmail.com>
2024-01-09 13:50:26 +0700

    Formatting code

--- workshop/demo/src/App.jsx
+++ workshop/demo/src/App.jsx
@@ -2,20 +2,16 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-
-  const [myName, setMyName] = useState('???')
-
+  const [myName, setMyName] = useState('???') // React Hooks
   function xxxx(event) {
     console.log(event.target.value)
     setMyName(event.target.value)
   }
-
   console.log("App render")
-
   return (
     <>
       <p>
-        Name :: <input type='text' onChange={xxxx} />
+        Name :: <input type='text' id="username_tf" onChange={xxxx} />
       </p>
       <p>
         Hello <span>{myName}</span>
