45bf5d514f68465f659a695abf9b69082bcfd7d7
up1 <somkiat.p@gmail.com>
2024-01-09 13:43:14 +0700

    Demo with event and useState

--- workshop/demo/src/App.jsx
+++ workshop/demo/src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+
+  const [myName, setMyName] = useState('???')
+
+  function xxxx(event) {
+    console.log(event.target.value)
+    setMyName(event.target.value)
+  }
+
+  console.log("App render")
+
   return (
     <>
       <p>
-        Name :: <input type='text' />
+        Name :: <input type='text' onChange={xxxx} />
       </p>
       <p>
-        Hello <span>????</span>
+        Hello <span>{myName}</span>
       </p>
     </>
   )
