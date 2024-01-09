f9ed2bfea0523adf64d88dcba9b0676fcd314668
up1 <somkiat.p@gmail.com>
2024-01-09 15:37:43 +0700

    Call api with axios + useEffect

--- workshop/demo/src/App.jsx
+++ workshop/demo/src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Welcome from './Welcome'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import axios from 'axios';
 
 function App() {
   const [myName, setMyName] = useState('???') // React Hooks
+
+  const [users, setUsers] = useState([]) // React Hooks
+  // Call API with axios https://jsonplaceholder.typicode.com/users
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        setUsers(res.data)
+      })
+  }, [])
 
   function handleClick(e) {
     setMyName(e.target.value)
   }
 
+  console.log(users)
+
   return (
     <>
+      <Button variant="outlined" >Get data from API</Button>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
       <Box
         component="form"
         sx={{
@@ -22,11 +41,11 @@ function App() {
         autoComplete="off"
       >
         <TextField id="outlined-basic" label="Your name" variant="outlined" onChange={handleClick} />
-        <Button variant="outlined" >Chage</Button>
       </Box>
       <p>
         <Welcome name={myName} />
       </p>
+      
     </>
   )
 }
