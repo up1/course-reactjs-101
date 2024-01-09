967d88413e3268f98e7211cece3d04f000b164c3
up1 <somkiat.p@gmail.com>
2024-01-09 16:53:44 +0700

    Mock api with cypress

--- workshop/demo/cypress/e2e/hello.spec.cy.js
+++ workshop/demo/cypress/e2e/hello.spec.cy.js
@@ -1,5 +1,18 @@
 describe('template spec', () => {
   it('passes', () => {
+
+    cy.intercept(
+      {
+        method: 'GET', // Route all GET requests
+        url: 'https://jsonplaceholder.typicode.com/users', // that have a URL that matches '/users/*'
+      },
+      [{
+        id: 1,
+        name: 'Somkiat',
+        email: 'xxx'
+      }] // and force the response to be: []
+    ).as('getUsers') // and assign an alias
+
     cy.visit('http://localhost:3000/')
     cy.get('#outlined-basic').should('have.value', '')
     cy.get('#outlined-basic').type('Somkiat')
