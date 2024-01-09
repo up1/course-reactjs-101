07af8f1abe236ddcd196ec129ec401d4cf9b533b
up1 <somkiat.p@gmail.com>
2024-01-09 16:42:07 +0700

    Fix test case

--- workshop/demo/cypress/e2e/hello.spec.cy.js
+++ workshop/demo/cypress/e2e/hello.spec.cy.js
@@ -3,6 +3,6 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000/')
     cy.get('#outlined-basic').should('have.value', '')
     cy.get('#outlined-basic').type('Somkiat')
-    cy.get('#result').should('have.text', 'Somkiat')
+    cy.get('#result').should('have.text', 'Hello Somkiat')
   })
 })
