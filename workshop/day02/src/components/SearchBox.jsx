d85ebfc276cf4bd734597d24d1401b7ae48c9eb9
up1 <somkiat.p@gmail.com>
2024-01-10 11:10:35 +0700

    Get post id from text field

--- workshop/day02/src/components/SearchBox.jsx
+++ workshop/day02/src/components/SearchBox.jsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
 const SearchBox = (props) => {
+    const [postId, setPostId] = useState(0) 
 
     function xyz() {
-      // TODO: Get post id from input
-      props.callback(123)
+      props.callback(postId)
     }
 
     return (
-      <div>≈
-        Post Id: <input type="text" /> 
+      <div>
+        Post Id: <input type="text" onChange={(e) => setPostId(e.target.value)} /> 
         <button onClick={xyz}>Get Post detail</button>
       </div>
     )
   }
+
+SearchBox.propTypes = {
+  callback: PropTypes.func.isRequired
+};
 
 export default SearchBox
