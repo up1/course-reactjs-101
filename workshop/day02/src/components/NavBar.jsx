9ae4ca0ca2ddab760a00ce2b77f121b0104f7c5c
up1 <somkiat.p@gmail.com>
2024-01-10 16:13:56 +0700

    Format code

--- workshop/day02/src/components/NavBar.jsx
+++ workshop/day02/src/components/NavBar.jsx
@@ -1,16 +1,16 @@
 import { NavLink } from 'react-router-dom';
 
-export const NavBar = () => {   
+export const NavBar = () => {
     return (
         <>
-          <div className={'nav-container'}>
-            <div>React Hooks</div>
-            <nav id="sidebar" className={'nav-item-container'}>
-              <NavLink to="/" className={"nav-item"}>Home</NavLink>
-              <NavLink to="/xyz">Xyz</NavLink>
-            </nav>
-          </div>
-          <hr />
+            <div className={'nav-container'}>
+                <div>React Hooks</div>
+                <nav id="sidebar" className={'nav-item-container'}>
+                    <NavLink to="/" className={"nav-item"}>Home</NavLink>
+                    <NavLink to="/xyz">Xyz</NavLink>
+                </nav>
+            </div>
+            <hr />
         </>
-      )
+    )
 }
