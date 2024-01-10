b28c3f4028b5cc379ab0c63e00cf2e4d03823633
up1 <somkiat.p@gmail.com>
2024-01-10 14:25:18 +0700

    Add session storage

--- workshop/day02/src/DemoContextProvider.jsx
+++ workshop/day02/src/DemoContextProvider.jsx
@@ -10,6 +10,8 @@ const DemoContextProvider = ({ children }) => {
     console.log("Hi from DemoContextProvider")
     // TODO :: write data to locatstorage vs session storage vs cookie secure
     localStorage.setItem("demo_data", JSON.stringify({msg: "Hello from context!"}))
+
+    sessionStorage.setItem("demo_data", JSON.stringify({msg: "Hello from context!"}))
   }
 
   return (
